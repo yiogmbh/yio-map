e4c9430839a868e686e53f6d7d4f114aed5aa597
FlorianFlatscher <florian.flatscher@gmail.com>
2025-07-21 16:45:22 +0200

    Update Yio Attribution

--- src/controls/YioAttribution.js
+++ src/controls/YioAttribution.js
@@ -7,7 +7,26 @@ img.alt = 'yio.at';
 img.style.height = '1em';
 
 export const YioAttribution = new Attribution({
-  attributions:
-    '<b><span style="margin-right: 3px;">|</span><a href="https://yio.at">yio.at</b></a>',
   label: img,
 });
+
+const originalRender = YioAttribution.render;
+YioAttribution.render = function (mapEvent) {
+  originalRender.call(this, mapEvent);
+
+  const container = this.element;
+  const list = container.querySelectorAll('.ol-attribution ul')[0];
+  if (list) {
+    const customHtml = `<a href="https://yio.at">yio gmbh</a><span style="margin-left: 4px; margin-right: 2px;">|</span>`;
+
+    // Remove existing custom entry if it exists
+    const existing = list.querySelector('li#yiomap-attribution');
+    if (existing) existing.remove();
+
+    // Add at the beginning
+    const li = document.createElement('li');
+    li.setAttribute('id', 'yiomap-attribution');
+    li.innerHTML = customHtml;
+    list.insertBefore(li, list.firstChild);
+  }
+};
